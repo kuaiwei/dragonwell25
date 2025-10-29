9e98e56bcbcb9c682a53632ee814312e58e20a1f
Matias Saavedra Silva <matsaave@openjdk.org>
2025-10-29 19:23:52 +0000

    8370217: [lworld] Crash in add_migrated_value_classes after merging JDK-8350550
    Reviewed-by: coleenp, fparain

--- src/hotspot/share/classfile/systemDictionary.cpp
+++ src/hotspot/share/classfile/systemDictionary.cpp
@@ -215,7 +215,7 @@ ClassLoaderData* SystemDictionary::register_loader(Handle class_loader, bool cre
       return ClassLoaderData::the_null_class_loader_data();
     } else {
       ClassLoaderData* cld = ClassLoaderDataGraph::find_or_create(class_loader);
-      if (EnableValhalla) {
+      if (Arguments::enable_preview() && EnableValhalla) {
         add_migrated_value_classes(cld);
       }
       return cld;
